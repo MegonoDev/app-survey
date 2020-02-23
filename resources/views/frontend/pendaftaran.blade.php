e3ce683de581df62ec0fe0214b45c54528cae090
Feri <ferifahrul1@gmail.com>
2020-02-23 22:43:12 +0700

    fix js eror console

--- resources/views/frontend/pendaftaran.blade.php
+++ resources/views/frontend/pendaftaran.blade.php
@@ -234,7 +234,7 @@
     </div>
   </div>
 
-  <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
   <script src="{{ asset('eonesia/b-n/assets/node_modules/bootstrap/dist/js/bootstrap.min.js')}}"></script>
   <script src="{{ asset('eonesia/b-n/assets/node_modules/jquery/jquery-3.2.1.min.js')}}"></script>
   <script src="{{ asset('js/bootstrap-datepicker.js') }}"></script>

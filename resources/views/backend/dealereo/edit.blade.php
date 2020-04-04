5b6d85f1fbcf1f0d146f0723bb34609194ff195d
Feri <ferifahrul1@gmail.com>
2020-04-04 15:50:43 +0700

    add form edit nama dealer

--- resources/views/backend/dealereo/edit.blade.php
+++ resources/views/backend/dealereo/edit.blade.php
@@ -5,6 +5,11 @@
     {!! Form::text('kode_dealer', null, ['id' => 'title', 'class' => 'form-control form-control-line', 'placeholder' => 'Dealer']) !!}
     {!! $errors->first('kode_dealer', '<p style="color:darkred" class="help-block">:message</p>') !!}
   </div>
+
+  <div class="col-md-12">
+    {!! Form::text('nama_dealer', null, ['id' => 'title', 'class' => 'form-control form-control-line', 'placeholder' => 'Nama Dealer']) !!}
+    {!! $errors->first('nama_dealer', '<p style="color:darkred" class="help-block">:message</p>') !!}
+  </div>
 </div>
 <div class="form-group">
   <div class="col-sm-12">

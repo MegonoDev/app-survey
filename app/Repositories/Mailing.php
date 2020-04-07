b7ae7659e8e750727b65e1664fd83157ab073ec1
ferifahrul13 <ferifahrul1@gmail.com>
2020-04-08 02:18:52 +0700

    change email sender

--- app/Repositories/Mailing.php
+++ app/Repositories/Mailing.php
@@ -9,7 +9,7 @@ class Mailing {
     {
         $send = Mail::send('email.verificationcode', ['code' => $data['kode']], function ($message) use ($data) {
             $message->subject('[Kode Konfirmasi] | YAMGROUP.ID');
-            $message->from('hello@eonesia.id', 'YAMGROUP.ID');
+            $message->from('hello@yamgroup.id', 'YAMGROUP.ID');
             $message->to($data['email']);
         });
         return $send;
